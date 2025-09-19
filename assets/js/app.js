51c693805773dda00c35105f3af494ba7c5aa5dd
Johntelle <jonatasfsjunior@gmail.com>
2025-09-18 21:31:52 -0300

    Teste

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,15 @@
 //
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
